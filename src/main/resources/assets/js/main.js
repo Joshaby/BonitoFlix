afd32abb9739cfe92b271ca352cfa51a888b506d
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-10-11 14:59:13 -0300

    Atualização nas telas de usuário, form.html e new.html, adicionado footer, checkbox de mostrar senha agora funciona. Inicío do desenvolvimento do index.html

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,3 +1,12 @@
+function changeVisibility() {
+    const input = document.getElementById("senha");
+    if (input.type === 'password') {
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
+}
+
 function voltar() {
     window.history.back();
 }

1d394d1831f821c1269371442cc70b86c7cc741f
italoneves <neves.italo@gmail.com>
2021-10-16 12:44:59 -0300

    Implementação do add no controller da serie.

--- src/main/java/br/edu/ifpb/domain/Serie.java
+++ src/main/java/br/edu/ifpb/domain/Serie.java
@@ -14,7 +14,6 @@ public class Serie implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String nome;
 
     @OneToMany(mappedBy = "serie")
--- src/main/java/br/edu/ifpb/repository/SerieRepository.java
+++ src/main/java/br/edu/ifpb/repository/SerieRepository.java
@@ -4,7 +4,11 @@ import br.edu.ifpb.domain.Serie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SerieRepository extends CrudRepository<Serie, Integer> {
 
+
+    Optional<Serie> findByNome(String nome);
 }

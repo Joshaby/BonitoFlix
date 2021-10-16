d6e1c7296b17a5a6d5c30015e2efe39281389037
italoneves <neves.italo@gmail.com>
2021-10-16 17:14:16 -0300

    implementação de consulta

--- src/main/java/br/edu/ifpb/repository/TemporadaRepository.java
+++ src/main/java/br/edu/ifpb/repository/TemporadaRepository.java
@@ -4,7 +4,11 @@ import br.edu.ifpb.domain.Temporada;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TemporadaRepository extends CrudRepository<Temporada, Integer> {
+
+    List<Temporada> findAll();
 
 }

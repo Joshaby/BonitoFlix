e283225bab35296dc0627d3a035797898a9608e1
italoneves <neves.italo@gmail.com>
2021-10-16 17:14:02 -0300

    implementação de consulta

--- src/main/java/br/edu/ifpb/repository/SerieRepository.java
+++ src/main/java/br/edu/ifpb/repository/SerieRepository.java
@@ -4,11 +4,13 @@ import br.edu.ifpb.domain.Serie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SerieRepository extends CrudRepository<Serie, Integer> {
 
-
     Optional<Serie> findByNome(String nome);
+
+    List<Serie> findAll();
 }
